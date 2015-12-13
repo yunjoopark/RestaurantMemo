35746b1e0729cd10d797e6b4d8fbc900c607bc56
Yunjoo Park <yunjoopark12@gmail.com>
2015-12-13 16:54:02 +0900

    add restaurant element xml and images

--- app/src/main/java/kr/ac/ewha/graduate/mobileprogramming/restaurantmemo/MainActivity.java
+++ app/src/main/java/kr/ac/ewha/graduate/mobileprogramming/restaurantmemo/MainActivity.java
@@ -1,13 +1,28 @@
 package kr.ac.ewha.graduate.mobileprogramming.restaurantmemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // set the activity with no title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
         setContentView(R.layout.activity_main);
     }
+
+    public void addRestaurant(View view) {
+
+    }
+
+
 }
