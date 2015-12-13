871207edd62a2465bc07b6bce0d63957c659b633
yunjoopark <yunjoopark12@gmail.com>
2015-12-13 19:06:49 +0900

    RestaurantInfo add

--- app/src/main/java/kr/ac/ewha/graduate/mobileprogramming/restaurantmemo/MainActivity.java
+++ app/src/main/java/kr/ac/ewha/graduate/mobileprogramming/restaurantmemo/MainActivity.java
@@ -1,15 +1,26 @@
 package kr.ac.ewha.graduate.mobileprogramming.restaurantmemo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RatingBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-//    RestaurantAdapter restaurantAdapter;
     ListView restaurantListView;
+    private ArrayList<RestaurantInfo> mRestaurantList;
+    private ArrayAdapter<RestaurantInfo> mRestaurantAdapterList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +37,51 @@ public class MainActivity extends Activity {
     }
 
     public void addRestaurant(View view) {
+        final RestaurantInfo newRes = new RestaurantInfo();
 
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Set Restaurant Name & Score");
+        dialog.setMessage("Input Restaurant Name and Score");
+
+        LinearLayout inputLayout = new LinearLayout(this);
+        inputLayout.setOrientation(LinearLayout.VERTICAL);
+
+        // 이름 입력
+        final EditText nameBox = new EditText(this);
+        nameBox.setHint("restaurantName");
+        inputLayout.addView(nameBox);
+
+        // 점수
+        final RatingBar rate = new RatingBar(this);
+        rate.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT
+                , ViewGroup.LayoutParams.WRAP_CONTENT));
+        rate.setNumStars(5);
+        rate.setStepSize((float) 0.5);   // 별 색을 몇개씩 올릴 것인가
+        rate.setRating((float) 3);       // 처음 별을 몇개로 표시할 것인가
+        inputLayout.addView(rate);
+
+        //final EditText scoreBox = new EditText(this);
+        //scoreBox.setHint("restaurantScore");
+        //layout.addView(scoreBox);
+
+        dialog.setView(inputLayout);
+
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                newRes.setName(nameBox.getText().toString());
+                newRes.setScore(rate.getRating());
+                //newRes.setScore(Integer.parseInt(scoreBox.getText().toString()));
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
     }
 
 
---
+++ app/src/main/java/kr/ac/ewha/graduate/mobileprogramming/restaurantmemo/RestaurantInfo.java
@@ -0,0 +1,83 @@
+package kr.ac.ewha.graduate.mobileprogramming.restaurantmemo;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+/**
+ * Created by Bak on 2015. 12. 13..
+ */
+
+
+
+public class RestaurantInfo implements Parcelable{
+    public String mName;
+    public float mScore;
+
+    static final int MAX_SCORE = 5;
+    static final String DEFAULT_NAME = "Restaurant";
+
+    public RestaurantInfo()
+    {
+        mName = DEFAULT_NAME;
+        mScore = 0;
+    }
+
+    public RestaurantInfo(String name, float score)
+    {
+        mName = name;
+        mScore = score;
+    }
+
+    protected RestaurantInfo(Parcel in) {
+        mName = in.readString();
+        mScore = in.readInt();
+    }
+
+    public static final Creator<RestaurantInfo> CREATOR = new Creator<RestaurantInfo>() {
+        @Override
+        public RestaurantInfo createFromParcel(Parcel in) {
+            return new RestaurantInfo(in);
+        }
+
+        @Override
+        public RestaurantInfo[] newArray(int size) {
+            return new RestaurantInfo[size];
+        }
+    };
+
+    public float getScore() {
+        return mScore;
+    }
+
+    public void setScore(float newScore) {
+        if(newScore > MAX_SCORE)
+        {
+            Log.d("INPUT_ERROR", "input invalid score");
+            return;
+        }
+
+        this.mScore = newScore;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String newName) {
+        this.mName = newName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mName);
+        dest.writeFloat(mScore);
+    }
+}
+
+
